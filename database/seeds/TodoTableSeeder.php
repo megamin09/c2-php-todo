bb1dec6ad7f5905685368bc0ea3f551e45b117c2
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-10-28 12:21:16 +0900

    seed add

--- database/seeds/TodoTableSeeder.php
+++ database/seeds/TodoTableSeeder.php
@@ -11,14 +11,30 @@ class TodoTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i <= 100; $i++){
+        for( $i = 1; $i <= 10; $i++ )
+        {
+            $str = "user000$i";
+            DB::table('users')->insert([
+                'id'=> $i,
+                'name' => "user$i",
+                'email' => "user$i@gmail.com",
+                'password' => bcrypt($str),
+            ]);
+        }
+
+        for($i = 1; $i <= 100; $i++)
+        {
+            $j = floor(($i - 1) / 10) + 1;
             DB::table('todos')->insert([
                 'title' => "タスク$i",
+                'user_id' => $j,
                 'due_date' => date('Y-m-d'),
                 'status' => 0,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ]);
         }
+
+        
     }
 }
