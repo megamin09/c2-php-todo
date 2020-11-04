0ee5bf4f3e1469dcc19e07bdd717c04b0465de29
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-04 10:27:44 +0900

    user private

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
+    // ページネーションの件数
+    private const PAGE_SIZE = 5;
     /**
      * Display a listing of the resource.
      * Todo一覧を取得
@@ -14,7 +17,7 @@ class TodoController extends Controller
      */
     public function index()
     {
-        $todo_list = Todo::paginate(5);
+        $todo_list = Auth::user()->todos()->paginate(self::PAGE_SIZE);
         return view('todo/index', compact('todo_list'));
     }
 
