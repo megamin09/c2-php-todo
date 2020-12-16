baddd5bdd3bbf82732f301914dc42f7fa48d784b
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-12-16 10:32:20 +0900

    date add

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -18,7 +18,7 @@
             var week_ja= new Array("日","月","火","水","木","金","土");
 
             //年・月・日・曜日を書き出す
-            document.write( "　　　　　　　　　　　　　　　　　" + year+"年"+month+"月"+day+"日 "+week_ja[week]+"曜日");
+            document.write( "　　　　　　　 　 　  　　　   　 　　　　" + year+"年"+month+"月"+day+"日 "+"("+week_ja[week]+")");
              </script> </h3>
 
 
